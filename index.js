8c36920174529480d78b8df1c744f162a1c80692
Backend cic <fundamentos.backend.2023@outlook.com>
2024-06-16 13:24:48 -0600

    modify: calling components from index

--- index.js
+++ index.js
@@ -1,76 +1,19 @@
 const express = require('express');
 const app = express();
 
+const configureRoutes = require('./routes');
+const { globalMw } = require('./middlewares/exampleMW');
+
 const port = process.env.PORT
 
 app.use(express.json());
-app.use((req, res, next) => {
-    console.log('this is a global mw');
-    next()
-});
-app.use('/clients', (req, res, next) => {
-    console.log('you are accessing clients mw');
-    next();
-});
+app.use(globalMw);
+// app.use('/clients', (req, res, next) => {
+//     console.log('you are accessing clients mw');
+//     next();
+// });
 
-const localMw = (req, res, next) => {
-    console.log('this is a local mw');
-    next();
-}
-
-const clients = {
-    c1: {
-        firstName: 'Miriam',
-        lastName: 'Hernandez',
-        spentAmount: '500'
-    }
-}
-
-app.get('/clients', (req, res) => {
-    res.status(200).json({
-        clients,
-        message: 'Here is the client list'
-    });
-});
-
-app.get('/clients/:id',
-    localMw,
-    (req, res) => {
-    const clientId = req.params.id;
-    const client = clients[clientId];
-    res.status(200).json({
-        client,
-        message: 'Here is the client list'
-    });
-});
-
-app.post('/clients', (req, res) => {
-    const info = req.body;
-    const clientsAmount =  (Object.keys(clients)).length + 1;
-    clients[clientsAmount] = info;
-    res.status(201).json({
-        message: 'client list updated'
-    });
-});
-
-app.patch('/clients/update-first-name/:id', (req, res) => {
-    const { id } = req.params;
-    const { firstName } = req.body;
-    clients[id].firstName = firstName;
-
-    res.status(200).json({
-        message: 'client updated'
-    });
-});
-
-app.delete('/clients/:id', (req, res) => {
-    const { id } = req.params;
-    delete clients[id]
-
-    res.status(200).json({
-        message: 'client deleted'
-    });
-});
+configureRoutes(app);
 
 
 app.get('/', (req, res) => {
